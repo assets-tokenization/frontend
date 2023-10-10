5fc1629a0f9f950f11397ad177f94896cdaa20c1
Oleksandr Klimovych <oleksandr@MacBook-Pro---Oleksandr.local>
2023-10-11 00:04:41 +0300

    remove unused

--- src/core/components/FilePreview/components/PdfDocument/index.jsx
+++
@@ -1,6 +0,0 @@
-import config from 'config';
-
-import OnePagePdfPreview from 'components/FilePreview/components/PdfDocument/OnePagePdfPreview';
-import DefaultPdfPreview from 'components/FilePreview/components/PdfDocument/DefaultPdfPreview';
-
-export default config?.variables?.onePagePdfPreview ? OnePagePdfPreview : DefaultPdfPreview;
